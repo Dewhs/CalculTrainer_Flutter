723bac0eb9b2bd3989b1277937d3552636e4f352
Dewhs <chevreuil.chauve@gmail.com>
2023-01-24 00:05:43 +0100

    alpha v0.352, last version

--- calcul_trainer/lib/View/CalculPage.dart
+++ calcul_trainer/lib/View/CalculPage.dart
@@ -303,26 +303,30 @@ class CalculPageState extends State<CalculPage> {
 class SymbolWidget extends StatelessWidget {
   const SymbolWidget({super.key, required this.id});
   final String id;
+
   @override
   Widget build(BuildContext context) {
-    if (id == "Addition") {
-      return const Text("  +  ", style: TextStyle(fontSize: 35));
-    } else if (id == "Substraction") {
-      return const Text("  -  ", style: TextStyle(fontSize: 35));
-    } else if (id == "Multiplication") {
-      return const Text("  x  ", style: TextStyle(fontSize: 35));
-    } else if (id == "Division") {
-      return const Text('  /  ', style: TextStyle(fontSize: 35));
-    } else if (id == "Random") {
-      var rdm = Random();
-      List<String> symbol = [
-        "Addition",
-        "Substraction",
-        "Multiplication",
-        "Division"
-      ];
-      operationId = symbol[rdm.nextInt(symbol.length)];
-      return SymbolWidget(id: operationId);
+    switch (id) {
+      case "Addition":
+        return const Text("  +  ", style: TextStyle(fontSize: 35));
+      case "Substraction":
+        return const Text("  -  ", style: TextStyle(fontSize: 35));
+      case "Multiplication":
+        return const Text("  x  ", style: TextStyle(fontSize: 35));
+      case "Division":
+        return const Text("  /  ", style: TextStyle(fontSize: 35));
+      case "Random":
+        {
+          var rdm = Random();
+          List<String> symbol = [
+            "Addition",
+            "Substraction",
+            "Multiplication",
+            "Division"
+          ];
+          operationId = symbol[rdm.nextInt(symbol.length)];
+          return SymbolWidget(id: operationId);
+        }
     }
 
     return const Text('is there a Little problem ?');
@@ -337,23 +341,26 @@ bool verifyReslut(String id, String _value) {
   double doubleValue = double.parse(_value);
   if (id == "Division") {
     return checkDivResult(doubleValue);
-  }
-  int value;
-  try {
-    value = int.parse(_value);
-  } catch (e) {
-    return false;
+  } else {
+    int value;
+    try {
+      value = int.parse(_value);
+    } catch (e) {
+      return false;
+    }
+
+    switch (id) {
+      case "Addition":
+        return checkAddResult(value);
+
+      case "Substraction":
+        return checkSubResult(value);
+
+      case "Multiplication":
+        return checkMultResult(value);
+    }
   }
 
-  if (id == "Addition") {
-    return checkAddResult(value);
-  }
-  if (id == "Substraction") {
-    return checkSubResult(value);
-  }
-  if (id == "Multiplication") {
-    return checkMultResult(value);
-  }
   return false;
 }
 
@@ -376,109 +383,3 @@ bool checkDivResult(double value) {
 bool checkMultResult(int value) {
   return (number1 * number2) == value;
 }
-
-
-
-
-/* 
-class OperationWidget extends StatefulWidget {
-  const OperationWidget(this.id, this.difficulty, this.actualiseWidget);
-  final Function actualiseWidget;
-
-  final String id;
-  final String difficulty;
-
-  @override
-  _OperationWidgetState createState() =>
-      _OperationWidgetState(this.id, this.difficulty, this.actualiseWidget);
-}
-
-class _OperationWidgetState extends State<OperationWidget> {
-  _OperationWidgetState(this.id, this.difficulty, this.actualiseWidget);
-  final id;
-  final difficulty;
-  final fieldText = TextEditingController();
-  final Function actualiseWidget;
-
-  void refreshOperation() {
-    var rdm = Random();
-    if (difficulty == "Easy") {
-      number1 = rdm.nextInt(9);
-      number2 = rdm.nextInt(9);
-    }
-    if (difficulty == "Intermediate") {
-      number1 = 10 + rdm.nextInt(89);
-      number2 = 10 + rdm.nextInt(89);
-    }
-    if (difficulty == "Expert") {
-      number1 = 100 + rdm.nextInt(899);
-      number2 = 100 + rdm.nextInt(899);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    refreshOperation();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (id == "Addition") {
-      return Row(
-        //Faudra créer un widget adapter à chaque operation
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            number1.toString(),
-            style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          ),
-          const Text("  +  ", style: TextStyle(fontSize: 35)),
-          Text(
-            number2.toString(),
-            style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          ),
-          const Text(
-            "  =  ",
-            style: TextStyle(fontSize: 35),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(245, 240, 240, 240),
-              borderRadius: BorderRadius.all(Radius.circular(25)),
-            ),
-            child: TextField(
-              focusNode: _focusNode,
-              autofocus: true,
-              controller: fieldText,
-              onSubmitted: (value) {
-                setState(() {
-                  nbCalculTotal++;
-                  if (verifyReslut(id, value)) nbCalculValidate++;
-                  _focusNode.requestFocus();
-                  refreshOperation();
-                  fieldText.clear();
-                  actualiseWidget();
-                });
-              },
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(top: 30.0),
-                hintText: '?',
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ], // Only numbers can be entered
-            ),
-          )
-        ],
-      );
-    }
-    return const Text("En cours de construction...");
-  }
-}*/
